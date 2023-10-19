637ce17a71e89c89db52f2e4225063417f03245c
AmeAya <deobackstep@gmail.com>
2023-10-19 20:05:13 +0600

    Email User

--- app/admin.py
+++ app/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(ExtendedUser)
+# admin.site.register(ExtendedUser)
+admin.site.register(EmailUser)
--- app/models.py
+++ app/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import EmailUserManager
 
 
 # AbstractUser - используем тогда, когда хотим добавить новые поля к встроенному Джанго Юзеру
-class ExtendedUser(AbstractUser):
-    iin = models.CharField(max_length=12, unique=True)
+# class ExtendedUser(AbstractUser):
+#     iin = models.CharField(max_length=12, unique=True)
+
+
+class EmailUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = EmailUserManager()
