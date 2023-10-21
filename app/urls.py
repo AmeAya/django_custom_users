a5bc7e9f00ab974dbe64c1f6e3b10bbc157e2257
AmeAya <deobackstep@gmail.com>
2023-10-21 19:58:43 +0600

    Login and Logout

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from django.contrib.auth.views import LoginView, LogoutView
+from .views import *
 
 urlpatterns = [
-    path('log_in/', LoginView.as_view(template_name='log_in.html'), name='log_in_url'),
-    path('log_out/', LogoutView.as_view(), name='log_out_url'),
+    path('log_in/', LogInView, name='log_in_url'),
+    path('log_out/', LogOutView, name='log_out_url'),
     path('about_us/', TemplateView.as_view(template_name='about_us.html'), name='about_us_url'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def LogInView(request):
+    if request.user.is_authenticated:
+        return redirect('about_us_url')
+    if request.method == 'GET':
+        return render(request=request, template_name='log_in.html')
+    elif request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(email=email, password=password)
+        # authenticate - Делает запрос в БД, ищет пользователя с таким username, хэширует пароль и сравнивает.
+        # Если данные верны, то мы получим объект user. Если нет, то None.
+        if user is not None:
+            login(request, user)
+            # login - регистрирует юзера в системе как юзера который уже вошел и выдает ему sessionid.
+            # sessionid и csrftoken Джанго хранит в БД в своей таблице. После выхода из системы, он их удаляет.
+            return redirect('about_us_url')
+        return redirect('log_in_url')
+
+
+def LogOutView(request):
+    if request.user.is_authenticated:
+        logout(request)
+        # logout - находит связанные с юзером sessionid и csrftoken и удаляет их из БД.
+    return redirect('log_in_url')
+
